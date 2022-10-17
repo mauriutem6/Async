c88d98bb317c4b14a3cc9ed222a54d1b5091bd26
mauriutem6 <mauriutem@gmail.com>
2022-10-17 15:34:39 -0400

    se agrega .net consola de procesos task, async, wait

---
+++ ASYN/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ASYN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool boolResult = false;
+            Task.Run(async () =>
+            {
+                Barman barman_ = new Barman();
+                Task<bool> return1 = barman_.CalientaSnack();
+                barman_.HacerCoctel();
+
+                boolResult = await return1;
+
+            }).GetAwaiter().GetResult();
+
+            if (boolResult) {
+                Console.WriteLine("el proceso termino correctamente.");
+            }
+
+        }
+
+
+        public class Barman
+        {
+            public async Task<bool> CalientaSnack()
+            {
+                Console.WriteLine("mete el snack al horno");
+                HttpClient cliente = new HttpClient();
+                await cliente.GetAsync("http://hdeleon.net");
+                Console.WriteLine("Saca el snack del horno");
+                return true;
+            }
+
+            public async Task HacerCoctel()
+            {
+                Console.WriteLine("comienza a hacer el coctel");
+                Thread.Sleep(3000);
+                Console.WriteLine("termina de hacer el coctel.");
+                Thread.Sleep(1000);
+            }
+
+
+        }
+
+    }
+}
